579483c67f4474577a82c1a403bbaa2bf45c035e
Jan Richter <falnyr@gmail.com>
2021-05-24 07:15:08 +1200

    feat: doctrine saving

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +28,29 @@ class QuestionController extends AbstractController
     public function homepage()
     {
         return $this->render('question/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Foobar');
+        $question->setSlug('foobar-'.rand(1,1000));
+        $question->setQuestion(<<<EOF
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et orci eu ligula posuere sodales ut vel odio. Quisque viverra, eros et elementum lobortis, dolor nisi varius nisi, id blandit ipsum felis at metus. Quisque nec est vel lacus elementum ornare et quis tortor. Morbi sollicitudin turpis at dictum luctus. In magna erat, tempus vitae porttitor eget, porta id tellus. Nulla facilisi. Maecenas porttitor iaculis felis nec rutrum. Proin gravida lacus non vulputate euismod. Phasellus sit amet ultrices nunc. Duis sollicitudin auctor maximus. Cras nec dapibus nisi. Proin tortor turpis, aliquet ac arcu posuere, pellentesque fermentum nisi.
+EOF
+);
+
+        if (rand(0,1)) {
+            $question->setAskedAt(new \DateTime(sprintf("-%d days", rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response($question->getId());
     }
 
     /**
